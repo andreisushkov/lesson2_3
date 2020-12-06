0abeab47b7397ded8fcff735a31eabce27f48331
Andreisushkov <Andreisushkov@mail.ru>
2020-12-06 19:08:22 +0300

    lesson2_3

---
+++ src/ru/geekbrains/new_project/Main.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.new_project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] mass = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < mass.length; i ++) {
+            if(i < 6)
+                i = i * 2;
+            System.out.println(i);
+
+        }
+    }
+    }
+
+
